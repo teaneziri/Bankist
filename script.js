d6e4ffe3aaab0740be510241b697c3a37b6ca433
teaneziri <neziritea@gmail.com>
2024-09-20 16:24:42 +0200

    Tabbed component, done

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
     e.preventDefault();
@@ -30,15 +33,15 @@ document.addEventListener('keydown', function (e) {
 });
 
 ////////////////
-const header=document.querySelector('.header');
-const allSections=document.querySelectorAll('.section');
-console.log(allSections);
+// const header=document.querySelector('.header');
+// const allSections=document.querySelectorAll('.section');
+// console.log(allSections);
 
-document.getElementById('#section--1');
-const allButtons=document.getElementsByName('button');
-console.log(allButtons);
+// document.getElementById('#section--1');
+// const allButtons=document.getElementsByName('button');
+// console.log(allButtons);
 
-console.log(document.getElementsByClassName('btn'));
+// console.log(document.getElementsByClassName('btn'));
 
 
 // //CREATING and SORTING elements
@@ -66,4 +69,30 @@ console.log(document.getElementsByClassName('btn'));
 
 // document.documentElement.style.setProperty('--color-primary','orangered');
 
+//--------------------------------
 
+
+//EVENT DELEGATION
+
+
+
+// Tabbed component
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
