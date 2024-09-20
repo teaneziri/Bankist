feb6e84eaca5e9f3a336cb2243de97701aad8a13
teaneziri <neziritea@gmail.com>
2024-09-20 17:13:48 +0200

    sticky navigation, done

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
+const nav=document.querySelector('.nav')
+
 
 const openModal = function (e) {
     e.preventDefault();
@@ -96,3 +98,42 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+//Menu fade animation
+nav.addEventListener('mouseover',function(e){
+    
+})
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+//STICKY NAVIGATION
+window.onscroll = function() {
+  stickyNavbar();
+};
+
+const navbar = document.querySelector(".nav");
+const sticky = navbar.offsetTop;
+
+
+function stickyNavbar() {
+  if (window.scrollY > sticky) {
+      navbar.classList.add("sticky");
+  } else {
+      navbar.classList.remove("sticky");
+  }
+}
